f858170786211df883d22ae0279449868149a3c5
Elponch31 <s22120189@alumnos.itsur.edu.mx>
2025-09-19 18:54:47 -0600

    Actualizacion del remember al rememberSaveable

--- app/src/main/java/com/example/basicstatecodelab_/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab_/WaterCounter.kt
@@ -11,23 +11,25 @@ import androidx.compose.foundation.layout.Column
 
 
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 
-import androidx.compose.runtime.remember
+
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 
 
+
+
+
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
-
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
-
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
