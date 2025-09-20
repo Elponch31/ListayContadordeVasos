d8fb0723d80f481c71631549a8001edfca172589
Elponch31 <s22120189@alumnos.itsur.edu.mx>
2025-09-19 20:37:33 -0600

    Fin

--- app/src/main/java/com/example/basicstatecodelab_/MainActivity.kt
+++ app/src/main/java/com/example/basicstatecodelab_/MainActivity.kt
@@ -3,16 +3,15 @@ package com.example.basicstatecodelab_
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import com.example.basicstatecodelab_.ui.theme.BasicStateCodelab_Theme
 
 
@@ -21,7 +20,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicStateCodelab_Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -33,4 +31,6 @@ class MainActivity : ComponentActivity() {
     }
 
 }
+
+
 
