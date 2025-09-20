b244163aaaec6fe7c7e0229918895a9dfe1d80d9
Elponch31 <s22120189@alumnos.itsur.edu.mx>
2025-09-19 18:40:25 -0600

    Actualizacion archivo WaterCounter

--- app/src/main/java/com/example/basicstatecodelab_/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab_/WaterCounter.kt
@@ -17,12 +17,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 
+
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
 
-        Text("You've had $count glasses.")
+        if (count > 0) {
+
+            Text("You've had $count glasses.")
+        }
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
